1e9b8c45eaae221f095bab152f062929a10806c4
Alex Paiano <alexpaiano14@outlook.com>
2024-03-25 21:14:02 -0300

    prueba

--- Class1.cs
+++ Class1.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace CalculadoraAgiles
 {
-    public class Class1
+    public class Calculadora
     {
+        public void Sumar()
+        {
+            
+        }
     }
 }
